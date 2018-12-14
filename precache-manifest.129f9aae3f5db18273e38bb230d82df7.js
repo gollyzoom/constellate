0969667ee36585b0016654bc12b13cb70eae5e14
Spider King <gollyzoom@gmail.com>
2018-12-13 18:03:44 -0800

    Updates

---
+++ precache-manifest.129f9aae3f5db18273e38bb230d82df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/constellate/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "198c6d639511d168240f",
+    "url": "/constellate/static/js/runtime~main.198c6d63.js"
+  },
+  {
+    "revision": "4f6e03b74f3faf5b8b96",
+    "url": "/constellate/static/js/main.4f6e03b7.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/constellate/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "4f6e03b74f3faf5b8b96",
+    "url": "/constellate/static/css/main.ad6d3220.chunk.css"
+  },
+  {
+    "revision": "a0292b0c437b97f500fc9c13cb10e2d7",
+    "url": "/constellate/index.html"
+  }
+];
